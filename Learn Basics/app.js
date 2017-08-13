4f4a353cf2fd37fbb756c6602b8d5c0d2e27b32e
Mir Ayman Ali <alimirayman@outlook.com>
2017-08-13 13:38:22 +0600

    tabs to spaces

--- Learn Basics/app.js
+++ Learn Basics/app.js
@@ -3,9 +3,9 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function greet (whattosay) {
-	return function (name) {
-		console.log(whattosay + ' ' + name)
-	}
+  return function (name) {
+    console.log(whattosay + ' ' + name)
+  }
 }
 
 var hello = greet('hello');
@@ -15,42 +15,41 @@ hello('Mitul');
 // Closures Part-2
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	function buildFunctions () {
-		var arr = [];
+function buildFunctions () {
+  var arr = [];
 
-		for (var i = 0; i < 3; i++) {
-			arr.push(
-				(function (j) {
-					return function () {
-						console.log(j);
-					}
-				}(i))
-			);
-		}
+  for (var i = 0; i < 3; i++) {
+    arr.push(
+      (function (j) {
+        return function () {
+          console.log(j);
+        }
+      }(i))
+    );
+  }
 
-		return arr;
-	}
+  return arr;
+}
 
-	var func =  buildFunctions();
+var func =  buildFunctions();
 
-	func[0]();
-	func[1]();
-	func[2]();
+func[0]();
+func[1]();
+func[2]();
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Closures Part-3
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	function genGreetings(language = '') {
-		return function (firstName = '', lastName = '') {
-			if (language === 'en') console.log( 'Hello, ' + firstName + ' ' + lastName )
-			else if ( language === 'es') console.log( 'Aloha, ' + firstName + ' ' + lastName )
-		}
-	}
+function genGreetings(language = '') {
+  return function (firstName = '', lastName = '') {
+    if (language === 'en') console.log( 'Hello, ' + firstName + ' ' + lastName )
+   else if ( language === 'es') console.log( 'Aloha, ' + firstName + ' ' + lastName )
+  }
+}
 
-	var greetEng = genGreetings('en');
-	var greetSpan = genGreetings('es');
+var greetEng = genGreetings('en');
+var greetSpan = genGreetings('es');
 
-	greetEng('Humaira', 'Ragman');
-	greetSpan('Google', 'Foobar');
-	
+greetEng('Humaira', 'Ragman');
+greetSpan('Google', 'Foobar');
